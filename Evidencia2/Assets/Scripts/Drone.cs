a9f0dd2ac5fb1adaa605247425e4be6646c03cf3
Jacob <a01643891@tec.mx>
2024-11-23 22:03:42 -0600

    Functions to make drone take off, patrol and land

--- Evidencia2/Assets/Scripts/Drone.cs
+++ Evidencia2/Assets/Scripts/Drone.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Drone : MonoBehaviour
 {
     Vector3 startPos;
+    bool move = false;
+    public float speed = 5f;
+
+    public Transform[] routeCheckpoints = new Transform[4];
+    Vector3 target;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +19,58 @@ public class Drone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space)){
+            takeOff();
+        }
+        if(Input.GetKeyDown(KeyCode.L)){
+            land();
+        }
+        if(Input.GetKeyDown(KeyCode.P)){
+            StartCoroutine(Patrol());
+        }
+    }
+
+    void FixedUpdate() {
+        float step = speed * Time.deltaTime;
+        if(move){
+            transform.position = Vector3.MoveTowards(transform.position, target, step);
+        }
+    }
+
+    public void takeOff(){
+        target = startPos;
+        target.y = startPos.y + 3;
+        move = true;
+        StartCoroutine(Stop());
+    }
+
+    public void land(){
+        target = startPos;
+        move = true;
+        StartCoroutine(Stop());
+    }
+
+    public IEnumerator Stop(){
+        while(Vector3.Distance(transform.position, target) != 0){
+            yield return Time.deltaTime;
+        }
+        move = false;
+    }
+
+    public IEnumerator Patrol(){
+        int patrolCheckpoint = 0;
+        target = routeCheckpoints[patrolCheckpoint].position;
+        move = true;
+        while(patrolCheckpoint < 4){
+            
+            target = routeCheckpoints[patrolCheckpoint].position;
+            while(Vector3.Distance(transform.position, target) != 0){
+                yield return Time.deltaTime;
+            }
+            yield return new WaitForSeconds(1);
+            patrolCheckpoint++;
+        }
+        move = false;
+        takeOff();
     }
 }
