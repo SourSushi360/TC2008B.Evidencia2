1ca41abe5644a491797f1f8bb9474a0f51bed906
Jacob <a01643891@tec.mx>
2024-11-27 22:37:55 -0600

    Added Spawners

---
+++ Evidencia2/Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject human;
+    public GameObject robot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spawnCharacter(int character){
+        //int chance = Random.Range(0, 11);
+        if(character > 8){
+            Instantiate(
+                    robot,
+                    this.transform
+                );
+        } else {
+            Instantiate(
+                    human,
+                    this.transform
+                );
+        }
+        
+    }
+}
